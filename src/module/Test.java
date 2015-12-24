dd00fad71363956a771848808076973b7aa176ae
alaeschaik <she15550@spengergasse.at>
2015-12-24 23:51:55 +0100

    onKeyPressLeft - looks good so far - added initializeValue() at the end.
    onKeyPressRight - needs to be implemented - I'll do it
    
    Test improved

--- src/module/Test.java
+++ src/module/Test.java
@@ -1,22 +1,52 @@
 package module;
 
+import java.util.Scanner;
+
 /**
  * Created by Biko on 23.12.2015.
  */
 public class Test {
-
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        Calc calc= new Calc(4);
-        calc.printTable();
-        System.out.println();
+        System.out.println("---------------------------- Test 2048 ----------------------------");
+        Calc calc = new Calc(4);
+        calc.initializeValue();
 
-        calc.onKeyPressLeftNew();
-        calc.printTable();
-        calc.onKeyPressLeftNew();
-        calc.printTable();
+        int choice = 0;
+        scanner = new Scanner(System.in);
+        while (choice != 1) {
+            calc.printTable();
+            System.out.println("8. UP");
+            System.out.println("2. DOWN");
+            System.out.println("4. LEFT");
+            System.out.println("6. RIGHT");
+            System.out.println("1. EXIT");
 
+            if (scanner != null)
+                choice = scanner.nextInt();
 
+            System.out.println();
 
+            if (choice <= 8) {
+                switch (choice) {
+                    case 1:
+                        System.out.println("Game exit");
+                        System.exit(0);
+                        break;
+                    case 2:
+                        //calc.onKeyPressDown(); break;
+                    case 4:
+                        calc.onKeyPressLeft();
+                        break;
+                    case 6:
+                        calc.onKeyPressRight();
+                        break;
+                    case 8:
+                        //calc.onKeyPressUp(); break;
+                }
+            }
+        }
+        System.out.println("-------------------------------------------------------------------");
     }
 }
